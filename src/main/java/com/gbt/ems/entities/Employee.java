e18ca678019ed214c5989efee9855a4aae827b67
Chinedu Okorie <okoriechinedusunday@gmail.com>
2024-11-08 16:25:58 +0100

    Feat: define employee entity

--- src/main/java/com/gbt/ems/entities/Employee.java
+++ src/main/java/com/gbt/ems/entities/Employee.java
@@ -1,5 +1,12 @@
 package com.gbt.ems.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,13 +14,21 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
 public class Employee {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
+  @Column(name = "last_name", nullable = false)
   private String lastName;
 
+  @Column(name = "first_name", nullable = false)
   private String firstName;
 
+  @Column(name = "email", nullable = false, unique = true)
   private String email;
 }
